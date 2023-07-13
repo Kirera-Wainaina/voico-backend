dbe4a23aaea1540fb325df247d4a4d5bdd488785
Kirera Wainaina <richardwainainak@gmail.com>
2023-07-13 17:34:01 +0300

    create a log function

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,3 +2,7 @@ exports.generateRandomName = function():string {
 
   return `${String(Math.round(Math.random() * 1e6))}-${Date.now()}`
 }
+
+exports.log = function(message: string) {
+  console.log(`${Date()}; ${message}`)
+}
