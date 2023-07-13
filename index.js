90eb4ef8cc1255b35985a05842b02828034f29a9
Kirera Wainaina <richardwainainak@gmail.com>
2023-07-13 12:18:04 +0300

    listen for requests on port 443

--- index.js
+++ index.js
@@ -2,16 +2,28 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const http = require("http");
 const http2 = require("http2");
+const fs = require('node:fs');
 const dotenv = require("dotenv");
 const HTTP_PORT = 80;
-const HTTPS_PORT = 443;
+const HTTP2_PORT = 443;
 dotenv.config();
-/////////////////////////REDIRECT HTTP REQUESTS////////////////////////////////////////
+///////////////////////// REDIRECT HTTP REQUESTS /////////////////////////////////
 const httpServer = http.createServer();
+httpServer.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
 httpServer.on("request", (request, response) => {
     response.writeHead(301, {
         "location": `${process.env.DOMAIN}${request.url}`
     });
     response.end();
 });
-httpServer.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
+//////////////////////// HANDLE HTTPS REQUESTS ///////////////////////////////////
+const options = {
+    allowHTTP1: true,
+    key: fs.readFileSync(process.env.CERT_KEY_PATH),
+    cert: fs.readFileSync(process.env.CERT_CERTIFICATE_PATH)
+};
+const http2Server = http2.createSecureServer(options);
+http2Server.listen(HTTP2_PORT, () => console.log(`Listening on port ${HTTP2_PORT}`));
+http2Server.on("request", (request, response) => {
+    console.log(request.headers);
+});
